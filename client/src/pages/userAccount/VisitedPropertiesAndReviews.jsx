176a23170d3e09cadce686db47a4f05c28aac4bb
cHANGTEEZY <jagatgurung30@gmail.com>
2024-12-18 17:52:20 +0545

    added visited properties section where user can review the visited property. Added a new table where visited property details are stored

--- client/src/pages/userAccount/VisitedPropertiesAndReviews.jsx
+++ client/src/pages/userAccount/VisitedPropertiesAndReviews.jsx
@@ -1,14 +1,13 @@
 import "./VisitedPropertiesAndReviews.css";
-
 import Header from "../../components/Header/Header";
 import Breadcrumb from "../../components/ui/BreadCrumb/BreadCrumb";
-
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 export default function VisitedPropertiesAndReviews() {
-  const [visitedProperties, setVisitedProperties] = useState([]); // Initialize as an empty array
+  const [visitedProperties, setVisitedProperties] = useState([]);
+  console.log(visitedProperties);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -69,24 +68,40 @@ export default function VisitedPropertiesAndReviews() {
                       </div>
                     ))}
                   </div>
-                  <div className="wishlist-description">
-                    <p>
-                      Location:{" "}
-                      <span>
-                        {property.approximate_location ?? "Not specified"}
-                      </span>
-                    </p>
-                    <p>
-                      Region:{" "}
-                      <span>{property.property_region ?? "Not specified"}</span>
-                    </p>
-                    <p>
-                      Bedrooms:{" "}
-                      <span>{property.bedrooms ?? "Not specified"}</span>
-                    </p>
-                    <p>
-                      Beds: <span>{property.beds ?? "Not specified"}</span>
-                    </p>
+                  <div className="visited-bottom">
+                    <div className="visited-description">
+                      <p>
+                        Location:{" "}
+                        <span>
+                          {property.approximate_location ?? "Not specified"}
+                        </span>
+                      </p>
+                      <p>
+                        Region:{" "}
+                        <span>
+                          {property.property_region ?? "Not specified"}
+                        </span>
+                      </p>
+                      <p>
+                        Bedrooms:{" "}
+                        <span>{property.bedrooms ?? "Not specified"}</span>
+                      </p>
+                      <p>
+                        Beds: <span>{property.beds ?? "Not specified"}</span>
+                      </p>
+                    </div>
+                    <Link
+                      to={`/property-review/${
+                        property.id
+                      }?propertyImage=${encodeURIComponent(
+                        property.images[0]
+                      )}&propertyTitle=${encodeURIComponent(property.title)}`}
+                      style={{
+                        textDecoration: "none",
+                      }}
+                    >
+                      <button>Give Review</button>
+                    </Link>
                   </div>
                 </div>
               ))
